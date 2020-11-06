ecaeaecf16505462a3d8205bc839371835a82b5b
seikosan <ionova_dasha123@mail.ru>
2020-11-06 13:34:11 +0400

    add mvc

---
+++ Summer_Practice/SummerPracticeProject.MVC/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SummerPracticeProject.BLL.Interfaces;
+
+namespace SummerPracticeProject.MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        private IUsersLogic userLogic;
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public UserController(IUsersLogic user)
+        {
+            userLogic = user;
+        }
+
+        public ViewResult Users()
+        {
+
+        }
+    }
+}
---
+++ Summer_Practice/SummerPracticeProject.MVC/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется следующим образом
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("SummerPracticeProject.MVC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SummerPracticeProject.MVC")]
+[assembly: AssemblyCopyright("© , 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("f1b0cfa1-a15a-46dd-b9be-4f8d6be010d5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
